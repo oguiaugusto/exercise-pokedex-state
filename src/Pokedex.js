3ff464f137c7b6c992564fdcf97128ba204f8d4d
Guilherme <guilhaugusto05@gmail.com>
2021-11-03 19:15:48 -0300

    Exercício 1

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,13 +2,59 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
-    render() {
-        return (
-            <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
-            </div>
-        );
+  constructor() {
+    super();
+
+    this.state = {
+      pokeIndex: 0,
     }
+
+    this.nextPokemon = this.nextPokemon.bind(this);
+    this.previousPokemon = this.previousPokemon.bind(this);
+  }
+  
+  
+  nextPokemon() {
+    const { pokemons } = this.props;
+    const { pokeIndex } = this.state;
+    if (pokeIndex !== pokemons.length - 1) {
+      this.setState((currentPokeIndex, _props) => ({
+        pokeIndex: currentPokeIndex.pokeIndex + 1,
+      }));
+    } else {
+      this.setState({
+        pokeIndex: 0,
+      });
+    }
+  }
+
+  previousPokemon() {
+    const { pokemons } = this.props;
+    const { pokeIndex } = this.state;
+    if (pokeIndex !== 0) {
+      this.setState((currentPokeIndex, _props) => ({
+        pokeIndex: currentPokeIndex.pokeIndex - 1,
+      }));
+    } else {
+      this.setState({
+        pokeIndex: pokemons.length - 1,
+      });
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <div className="pokedex">
+          <Pokemon pokemon={this.props.pokemons[this.state.pokeIndex]} />
+        </div>
+        <div className="buttons">
+          <button onClick={this.previousPokemon}>Anterior</button>
+          <button onClick={this.nextPokemon}>Próximo</button>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Pokedex;
